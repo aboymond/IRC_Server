9f380ab30b47c4eedb47831e22e5673501cc90b7
aboymond <aboymond@student.42lausanne.ch>
2023-08-25 15:51:50 +0200

    refonte de tous

---
+++ user.cpp
@@ -0,0 +1,51 @@
+#include "user.hpp"
+
+User::User() : _moderator(false) {};
+User::~User() {};
+User::User(User const &src){
+	*this = src;
+}
+User &User::operator=(User const &rhs){
+	if (this != &rhs){
+		_nickName = 	rhs._nickName;
+		_userName = 	rhs._userName;
+		_socket_user = 	rhs._socket_user;
+		_moderator = 	rhs._moderator;
+	}
+	return (*this);
+}
+// SET
+
+void		User::setNickName(const std::string &nickname){
+	_nickName = nickname;
+}
+
+void		User::setUserName(const std::string &username){
+	_userName = username;
+}
+
+void		User::setSocketUser(int socket_user){
+	_socket_user = socket_user;
+}
+
+void		User::setOperator(bool moderator){
+	_moderator = moderator;
+}
+
+// GET
+
+const std::string &User::getNickName() const{
+	return (_nickName);
+}
+
+const std::string &User::getUserName() const{
+	return (_userName);
+}
+
+int			User::getSocketUser() const{
+	return (_socket_user);
+}
+
+bool		User::getOperator() const{
+	return (_moderator);
+}
---
+++ user.hpp
@@ -0,0 +1,42 @@
+#ifndef USER_HPP
+#define USER_HPP
+
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <sstream>
+#include "client.hpp"
+
+
+class User {
+
+private:
+	Client		*_client;
+
+	std::string	_nickName;
+	std::string	_userName;
+	int			_socket_user;
+	bool		_moderator;
+
+public:
+
+	User();
+	User(User const &src);
+	User &operator=(User const &rhs);
+	~User();
+
+	// SET
+	void				setNickName(const std::string &nickname);
+	void				setUserName(const std::string &username);
+	void				setSocketUser(int socket_user);
+	void				setOperator(bool moderator);
+
+	// GET
+	const std::string	&getNickName() const;
+	const std::string	&getUserName() const;
+	int					getSocketUser() const;
+	bool				getOperator() const;
+
+};
+
+#endif
